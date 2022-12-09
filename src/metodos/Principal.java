ba208fe65ceb4c230c6aa8a3accb710d9f356c6a
Jorge Lucas <jorge.santos@bitso.com>
2022-12-09 20:24:24 -0300

    metodos

--- src/metodos/Principal.java
+++ src/metodos/Principal.java
@@ -1,4 +1,34 @@
 package metodos;
 
+import java.util.Scanner;
+
 public class Principal {
+
+    public static void main(String [] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        int resultado = somarNumeros("informe o numero: ", ler);
+
+        System.out.println("Resultado: "+resultado);
+
+    }
+
+
+    public static int somarNumeros(String mensagem, Scanner ler) {
+        System.out.println(mensagem);
+        int primeiro = ler.nextInt();
+
+        System.out.println(mensagem);
+        int segundo = ler.nextInt();
+
+        return primeiro + segundo;
+    }
+
+    // [modificador]: public, protected e private
+    // [tipo retorno]: int, long, Pessoa, Integer
+    // nome         : somarTresNumeros
+    // ()/(tipo nome) : somarTresNumeros(int numeroUm, int numero2, int numero3)
+    // {}
+
 }
