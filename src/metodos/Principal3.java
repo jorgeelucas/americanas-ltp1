9c3231c8ffbc711bd4375bd8de8db4cc32c3d154
Jorge Lucas <jorge.santos@bitso.com>
2022-12-09 22:01:33 -0300

    principal 3

--- src/metodos/Principal3.java
+++ src/metodos/Principal3.java
@@ -17,18 +17,41 @@ public class Principal3 {
         System.out.println("add (min): ");
         int minutoAdd = scanner.nextInt();
 
-        addMinuto(hora, minuto, minutoAdd);
+        HoraEMinuto fusoBrasil = calcularHorario(hora, minuto, minutoAdd, 0, "Brasil");
+        HoraEMinuto fusoMexico = calcularHorario(hora, minuto, minutoAdd, -3, "Mexico");
+        HoraEMinuto fusoLondres = calcularHorario(hora, minuto, minutoAdd, 3, "Londres");
+
+        fusoBrasil.imprimir();
+        fusoMexico.imprimir();
+        fusoLondres.imprimir();
     }
 
-    public static void addMinuto(int hora, int minuto, int minAdd) {
-        int minutoRestante = (minuto + minAdd) % 60;
-        int horaAdicionada = (hora + (minuto + minAdd) / 60);
-        fusoHorario(horaAdicionada, minutoRestante);
-    }
+    private static HoraEMinuto calcularHorario(int hora, int minuto, int minutoAdd, int fuso, String fusoTxt) {
+        // calcular a qtd de horas que vão ser adicionadas pelos minutos
+        int horasParaAddNaHora = (minuto + minutoAdd) / 60;
 
-    private static void fusoHorario(int hora, int minuto) {
-        System.out.printf("Brasil: %dh%dmin\n", hora % 24, minuto);
-        System.out.printf("Mexico: %dh%dmin\n" , ((hora - 3) +24) % 24, minuto);
-        System.out.printf("londres: %dh%dmin\n", ((hora + 3) +24) % 24, minuto);
+        // somei tudo na hora e adicionei 24 para rodar o relogio e peguei o resto com 24
+        int novaHora = (hora + fuso + horasParaAddNaHora + 24) % 24;
+
+        // pegando os minutos dentro do intervalo de 0 a 59
+        int novoMinuto = (minuto + minutoAdd) % 60;
+
+
+        HoraEMinuto resultado = new HoraEMinuto();
+        resultado.hora = novaHora;
+        resultado.minuto = novoMinuto;
+        resultado.fuso = fusoTxt;
+
+        return resultado;
+    }
+}
+
+class HoraEMinuto {
+    int hora;
+    int minuto;
+    String fuso;
+
+    public void imprimir() {
+        System.out.println(hora+ " h " + minuto + " min " + "("+ fuso+")");
     }
 }
