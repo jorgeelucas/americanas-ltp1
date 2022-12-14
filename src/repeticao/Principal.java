0366e2711cbf0a70dab08d0305ef4df3f2da22fc
Jorge Lucas <jorge.santos@bitso.com>
2022-12-14 19:45:55 -0300

    loop

---
+++ src/repeticao/Principal.java
@@ -0,0 +1,20 @@
+package repeticao;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        /**
+         * Exercício
+         *
+         * 1. Faça um programa que leia 5 números e informe o maior número.
+         *
+         * 2. Faça um programa que receba dois números inteiros e
+         *      gere os números inteiros que estão no
+         *      intervalo compreendido por eles.
+         *
+         */
+
+    }
+
+}
